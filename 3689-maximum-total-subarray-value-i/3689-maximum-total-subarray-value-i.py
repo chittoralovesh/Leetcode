004da5842bf3890f3608a19f0320bdaff6fbcfae
Lovesh Chittora <chittoralovesh@gmail.com>
2026-06-09 21:55:59 +0530

    Time: 12 ms (62.4%), Space: 26.4 MB (88.8%) - LeetHub

---
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def maxTotalValue(self, nums: List[int], k: int) -> int:
+        mx = max(nums)
+        mn = min(nums)
+        return (mx - mn) * k
